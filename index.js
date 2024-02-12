535857ab82f420cbcff1b03e274fce8f41b40270
Manuel Cota <cota.manuel@rocketmail.com>
2024-02-11 18:35:28 -0700

    Added headers to RT POST for CORS

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ app.get('/rtsol', (req, res) => {
 });
 
 app.post('/rtsol', (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+      )
     const { sol } = req.body;
 
     digit1 = sol.digit1
